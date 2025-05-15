b4b728afd2157df81604b6155eb0a7f8ce08b84e
Kevin Franco Guerrero <kevinfrancoguerrero@Kevins-MacBook-Pro-2.local>
2025-05-15 07:47:58 -0500

    add: structure project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-center py-8 bg-red-100">
+        Bienvenido a Shopi
+      </h1>
+    </div>
+  )
+}
+
+export default App
